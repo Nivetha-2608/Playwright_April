a0677ee62a7fd05997de9b08b667994b344cf061
Nivetha-2608 <nivethag2608@gmail.com>
2026-09-07 12:04:39 +0530

    Second Commit Tester1

--- pages/loginpagelocator.js
+++ pages/loginpagelocator.js
@@ -1,25 +1,23 @@
-const { BaseReuse } = require('../utility/base')
-const { baseurl, username, password } = require('../utility/env')
+const { BaseReuse } = require("../utility/base");
+const { baseurl, username, password } = require("../utility/env");
 
 class LocatorPage {
-    constructor(page) {
-        this.page = page;
+  constructor(page) {
+    this.page = page;
 
-        //locators
-        this.usernameInput = "#username";
-        this.passwordInput = "#password";
-        this.loginButton = "#login"
-    }
+    this.usernameInput = "#username";
+    this.passwordInput = "#password";
+    this.loginButton = "#login";
+  }
 
-    async navigate() {
-        await BaseReuse.navigateToUrl(this.page, baseurl);
-    }
+  async navigate() {
+    await BaseReuse.navigateToUrl(this.page, baseurl);
+  }
 
-    async login(){
-        await BaseReuse.fillInput(this.page, this.usernameInput,username);
-        await BaseReuse.fillInput(this.page, this.passwordInput,password);
-        await BaseReuse.clickElement(this.page,this.loginButton );
-    }
-
+  async login() {
+    await BaseReuse.fillInput(this.page, this.usernameInput, username);
+    await BaseReuse.fillInput(this.page, this.passwordInput, password);
+    await BaseReuse.clickElement(this.page, this.loginButton);
+  }
 }
-module.exports={LocatorPage};
+module.exports = { LocatorPage };
